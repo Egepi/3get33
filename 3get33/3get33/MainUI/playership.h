4485d760d3417e3691005a8751901a784a7d5c68
jenniferk89 <jenniferk89@12f3ad18-97d0-11de-8bb7-81ed9af7660d>
2009-10-07 15:32:50 +0000

    BUILD FIXED.

--- 3get33/3get33/MainUI/playership.h
+++ 3get33/3get33/MainUI/playership.h
@@ -11,13 +11,10 @@ class PlayerShip : public BasicShip
 {
 public:
     PlayerShip();
-    void PlayerShip::keyPressEvent(QKeyEvent*);
     void PlayerShip::advanceLeft();
     void PlayerShip::advanceRight();
     void PlayerShip::advanceUp();
     void PlayerShip::advanceDown();
-private:
-    QKeyEvent *key;
 };
 
 #endif // PLAYERSHIP_H
